556fabc5d2872342bfb66eaed1435eb362e40b1c
Dumie1 <dumiedumara@gmail.com>
2018-09-16 15:46:22 +0200

    added more code

--- week1/index.js
+++ week1/index.js
@@ -18,6 +18,13 @@ function fetchJSON(url, cb) {
     xhr.send();
 }
 
+function createAndAppend(name, parent) {
+    const elem = document.createElement(name);
+    parent.appendChild(elem);
+    return elem;
+
+}
+
 fetchJSON('https://api.github.com/orgs/HackYourFuture/repos?per_page=100', (error, response) => { 
     if (error !== null) {
         alert('error');
@@ -29,6 +36,7 @@ fetchJSON('https://api.github.com/orgs/HackYourFuture/repos?per_page=100', (erro
         header.id = 'subRoot';
         const heading = createAndAppend('h1', header);
         heading.innerHTML = 'HYF Repositories :';
+
         const select = createAndAppend('select', header);
         select.setAttribute('class', 'select_menu');
         const optionItem = createAndAppend('option', select);
@@ -39,36 +47,73 @@ fetchJSON('https://api.github.com/orgs/HackYourFuture/repos?per_page=100', (erro
             optionItem.innerText = obj.name;
             optionItem.value = 'https://api.github.com/repos/HackYourFuture/' + obj.name;
         }
-        
+        select.addEventListener('change', request => {
+            const newUrl = event.target.value;
+            const root = document.getElementById('root');
+
+            root.innerHTML = null;
+            fetchJSON("https://api.github.com/orgs/HackYourFuture/repos?per_page=100", newUrl);
+            fetchJSON(newUrl, repoInfo);
+        });
     }
 });
 
+function repoInfo(error, data) {
+    if (error !== null) {
+        console.error(error);
+    } else {
+        const repositoryDetails = (data);
+        console.log(repositoryDetails);
+        const root = document.getElementById('root');
+        const informationContainer = createAndAppend('div', root);
+        informationContainer.id = 'repo_information';
 
-
-function createAndAppend(name, parent){
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    return elem;
-
+        const table = createAndAppend('table', informationContainer);
+        const tr1 = createAndAppend('tr', table);
+        const th1 = createAndAppend('th', tr1);
+        const td1 = createAndAppend('td', tr1);
+        const pageLink = createAndAppend('a', td1);
+        pageLink.innerHTML = repositoryDetails.name;
+        pageLink.setAttribute('href', repositoryDetails.svn_url);
+        pageLink.setAttribute('target', '_blank');
+        const tr2 = createAndAppend('tr', table);
+        const th2 = createAndAppend('th', tr2);
+        const td2 = createAndAppend('td', tr2);
+        td2.innerHTML = repositoryDetails.description;
+        const tr3 = createAndAppend('tr', table);
+        const th3 = createAndAppend('th', tr3);
+        const td3 = createAndAppend('td', tr3);
+        td3.innerHTML = repositoryDetails.forks;
+        const tr4 = createAndAppend('tr', table);
+        const th4 = createAndAppend('th', tr4);
+        const td4 = createAndAppend('td', tr4);
+        th1.innerHTML = 'Repository :';
+        th2.innerHTML = 'Description :';
+        th3.innerHTML = 'Forks :';
+        th4.innerHTML = 'Updated :';
+        td4.innerHTML = repositoryDetails.updated_at;
+        const contributorsUrl = repositoryDetails.contributors_url;
+        fetchJSON(contributorsUrl, getContributors);
+    }
 }
 
-
-
-
-
-// fetchJSON('https://api.github.com/orgs/HackYourFuture/repos?per_page=100',(error, data) => {
-//     if (error == null) {
-//         const repos = response.data;
-//         const root = document.getElementById('root');
-//         const ul = createAndAppend('ul', root);
-
-//         for (let i = 0; i < data.length; i++){
-//             const repo = data(i);
-//             const li = createAndAppend('li', ul);
-//             li.innerHTML = repo.name + '' + repo.id;
-//         }
-//     } else {
-//         console, log(error);
-//     }
-// });
-
+function getContributors(error, data) {
+    if (error !== null) {
+        console.error(error);
+    } else {
+        const repoData = (data);
+        const root = document.getElementById('root');
+        const showContributors = createAndAppend('div', root);
+        showContributors.setAttribute('id', 'contributors');
+        const contributorsHeading = createAndAppend('h2', showContributors);
+        contributorsHeading.innerHTML = 'Contributions';
+        for (const contributor of repoData) {
+            const contributorName = createAndAppend('h3', showContributors);
+            const contributorLink = createAndAppend('a', contributorName);
+            contributorLink.innerHTML = contributor.login;
+            const contributorImage = createAndAppend('img', showContributors);
+            contributorImage.setAttribute('class', 'contributor_img');
+            contributorImage.setAttribute('src', contributor.avatar_url);
+        }
+    }
+}
